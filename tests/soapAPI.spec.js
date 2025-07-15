7956d308db1f5f560fc3a072e79c197e92cad1ee
Rakib Hasan <cse.rakib@gmail.com>
2025-07-16 01:26:08 +0600

    added a new soupAPI test spec

---
+++ tests/soapAPI.spec.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const { XMLParser } = require('fast-xml-parser');
+const expect = require('chai').expect;
+
+describe('SOAP API Test - Add Operation', () => {
+  it('should return 15 as the result of 10 + 5', async () => {
+    const soapEnvelope = `
+      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+          <Add xmlns="http://tempuri.org/">
+            <intA>10</intA>
+            <intB>5</intB>
+          </Add>
+        </soap:Body>
+      </soap:Envelope>
+    `;
+
+    const response = await request('http://www.dneonline.com')
+      .post('/calculator.asmx')
+      .set('Content-Type', 'text/xml; charset=utf-8')
+      .set('SOAPAction', '"http://tempuri.org/Add"')
+      .send(soapEnvelope);
+
+    const parser = new XMLParser();
+    const json = parser.parse(response.text);
+    const result = json['soap:Envelope']['soap:Body']['AddResponse']['AddResult'];
+
+    await expect(response.statusCode).to.equal(200);
+    await expect(response.ok).to.equal(true);
+    await expect(result).to.equal(15);
+  });
+});

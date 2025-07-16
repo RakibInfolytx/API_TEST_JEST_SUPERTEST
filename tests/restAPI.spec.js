d3cd57714d555fbd73880e83df127d47115ddf9b
Rakib Hasan <cse.rakib@gmail.com>
2025-07-16 14:11:37 +0600

    rest api url changed

--- tests/restAPI.spec.js
+++ tests/restAPI.spec.js
@@ -2,35 +2,62 @@ const request = require('supertest');
 
 const expect = require('chai').expect;
 
-const baseUrl = 'https://reqres.in';
+const restBaseUrl = 'https://api.restful-api.dev';
 
 describe('- REST API Testing', () => {
 	
-  it('- Should be able to verify (GET) USER', async () => {
+  it('- Should be able to verify (GET) object', async () => {
 
-    const response = await request(baseUrl).get('/api/users?page=2');
+    const response = await request(restBaseUrl).get('/objects?id=3');
     await expect(response.statusCode).to.equal(200);
     await expect(response.ok).to.equal(true);
-
-	});
-
-  xit('- Should be able to creat(POST) USER', async () => {
-
-    const response = await request(baseUrl)
-    .post('/api/users')
-    .send({"name": "morpheus","job": "leader"});
-    await expect(response.statusCode).to.equal(201);
-    await expect(response.ok).to.equal(true);
+    await expect(response._body[0].id).to.equal("3");
+    await expect(response._body[0].name).to.equal('Apple iPhone 12 Pro Max');
+    await expect(response._body[0].data.color).to.equal("Cloudy White");
     
 	});
 
-  xit('- Should be able to update(PUT) USER', async () => {
+  it('- Should be able to creat(POST) object', async () => {
 
-    const response = await request(baseUrl)
-    .put('/api/users/2')
-    .send({"name": "morpheus","job": "zion resident"});
+    const response = await request(restBaseUrl)
+    .post('/objects')
+    .send({
+      "name": "Apple MacBook Pro 16",
+      "data": {
+        "year": 2019,
+        "price": 1849.99,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB"
+      }
+    });
+
+    console.log(response._body.data)
     await expect(response.statusCode).to.equal(200);
     await expect(response.ok).to.equal(true);
+    await expect(response._body.data.year).to.equal(2019);
+    await expect(response._body.data.price).to.equal(1849.99);
+    
+	});
+
+  it('- Should be able to update(PUT) object', async () => {
+
+    const response = await request(restBaseUrl)
+    .put('/objects/ff8081819782e69e0198123e3d462b65')
+    .send({
+      "name": "Apple MacBook Pro 16",
+      "data": {
+        "year": 2025,
+        "price": 20049.99,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB",
+        "color": "silver"
+      }
+    });
+
+    await expect(response.statusCode).to.equal(200);
+    await expect(response.ok).to.equal(true);
+    await expect(response._body.data.year).to.equal(2025);
+    await expect(response._body.data.price).to.equal(20049.99);
     
 	});
 
